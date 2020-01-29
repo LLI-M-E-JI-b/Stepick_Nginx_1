57395885f570e2ee7e343a80c4e9c23d517f91a2
SHMEL <alexandrbasu@gmail.com>
2020-01-29 19:06:36 +0300

    hz

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn --bind='0.0.0.0:8080' hello:app
+
+sudo rm -r /etc/gunicorn.d/*
+sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
